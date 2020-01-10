a9b2e7663fdd200b8101fdbf106ba310559ec903
sercan çetin <sercancetin@yandex.com>
2020-01-10 16:58:49 +0300

    alarm notification eklendi

--- app/src/main/java/com/smart/smarthome/service/AlarmService.java
+++ app/src/main/java/com/smart/smarthome/service/AlarmService.java
@@ -1,10 +1,17 @@
 package com.smart.smarthome.service;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.smart.smarthome.R;
 import com.smart.smarthome.helper.AlarmTaskHelper;
 
 import java.text.SimpleDateFormat;
@@ -20,5 +27,18 @@ public class AlarmService extends BroadcastReceiver {
         Log.d("authorize","AlarmService: Sistem saat: "+currentTime);
         alarmTaskHelper.doTheTask(currentTime);
         alarmTaskHelper.deleteHourAlarm(currentTime);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("kanal_id","kanalad", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("kanal_desc");
+            NotificationManager manager = context.getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"kanal_id")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Smart Home")
+                .setContentText("Ayarladığınız görevler gerçekleştirildi.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(1,builder.build());
     }
 }
